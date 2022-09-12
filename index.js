99e638b24cb71f594316eb78cad7b9d800051e77
I5van <n5im@yandex.ru>
2022-09-12 16:30:42 +0300

    3

---
+++ index.js
@@ -0,0 +1,24 @@
+// let m =  Number (prompt('Введите число денег Даши'));
+// let n = Number (prompt ('Введите число денег Маши'));
+// let k = (m - n)/2;
+// let f = (n - m)/2;
+// if(m > n){
+//     alert(`Даша даст Маше ${k} рублей, и у обеих девочек станет ${m-k} рублей.`)
+// }
+// else{
+//     alert(`Маша даст Даше ${f} рублей, и у обеих девочек станет ${n-f} рублей.`)
+// }
+
+
+Math.floor(Math.random()*100)
+let i=0;
+
+while(i<10){
+   let n= Math.floor(Math.random()*100)
+    console.log(n)
+    i+=1;
+      
+}
+let k=n/10
+    console.log ()
+
