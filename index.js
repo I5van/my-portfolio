398c80e1dc31ec7d05727abb1c14ff2c56c0d03c
I5van <n5im@yandex.ru>
2022-09-12 17:06:13 +0300

    5

--- index.js
+++ index.js
@@ -9,16 +9,26 @@
 //     alert(`Маша даст Даше ${f} рублей, и у обеих девочек станет ${n-f} рублей.`)
 // }
 
+// доделать
 
-Math.floor(Math.random()*100)
-let i=0;
+// Math.floor(Math.random()*100)
+// let i=0;
 
-while(i<10){
-   let n= Math.floor(Math.random()*100)
-    console.log(n)
-    i+=1;
+// while(i<10){
+//    let n= Math.floor(Math.random()*100)
+//     console.log(n)
+//     i+=1;
       
-}
-let k=n/10
-    console.log ( )
+// }
+// let k=n/10
+//     console.log ( )
+
+let n = Number(prompt("Введите шестизначное число"));
+let a = Math.floor(n/100000);
+let b = Math.floor(n/10000)%10;
+let c = Math.floor(n/1000)%10;
+let d = Math.floor(n/100)%10;
+let e = Math.floor(n/10);
+let f = n%10;
+alert(`${a} ${b} ${c} ${d} ${e} ${f}`)
 
