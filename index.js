7c6d8f286aaf340e36f98cae6c6c595c5355e25c
I5van <n5im@yandex.ru>
2022-09-18 17:32:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,12 +23,40 @@
 // let k=n/10
 //     console.log ( )
 
-let n = Number(prompt("Введите шестизначное число"));
-let a = Math.floor(n/100000);
-let b = Math.floor(n/10000)%10;
-let c = Math.floor(n/1000)%10;
-let d = Math.floor(n/100)%10;
-let e = Math.floor(n/10);
-let f = n%10;
-alert(`${a} ${b} ${c} ${d} ${e} ${f}`)
+// let n = Number(prompt("Введите шестизначное число"));
+// let a = Math.floor(n/100000);
+// let b = Math.floor(n/10000)%10;
+// let c = Math.floor(n/1000)%10;
+// let d = Math.floor(n/100)%10;
+// let e = Math.floor(n/10)%10;
+// let f = n%10;
+// // alert(`${a} ${b} ${c} ${d} ${e} ${f}`)
+// if((a+b+c)==(d+e+f)){
+//     alert("Число счастливое")
+// }
+// else{
+//     alert("Число несчастливое")
+// }
+
+// let k = Number(prompt("Введите число"));
+// let i = k-10;
+// let f = k+11;
+//     while(i<f){
+//     i+=1
+//     console.log(i);
+// }
+let s = Number(prompt("Введите сумму"));
+let k = Number(prompt("Введите время"));
+let p = Number(prompt("Введите проценты годовых"));// 
+let i = 0;
+while(i<k){
+s=s+(s/100*p)
+
+ i+=1
+
+
+ 
+}
+console.log(s)
+
 
